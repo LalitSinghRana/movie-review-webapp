654b84faf5ee85384a4af2f581c291f0acfcbf7e
NAGARRO\lalitrana <lalit.rana@nagarro.com>
2022-10-22 14:12:14 +0530

    added type definations in index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,22 +8,24 @@ import cors from 'cors';
 import dbConnection from '@database/Config';
 import movieRoutes from '@routes/movie-routes';
 import reviewRoutes from '@routes/review-routes';
+import defaultJson from '@config/default.json';
 
 const app = express();
 
 app.use(cors<Request>());
 app.use(express.json());
+
 app.use('/movie', movieRoutes);
 app.use('/review', reviewRoutes);
 
-const alterSchema: boolean = config.get('AlterSchema');
+const alterSchema = config.get<typeof defaultJson.AlterSchema>('AlterSchema');
 console.log(`Altering schemas on this run: ${alterSchema}`);
 dbConnection
 	.sync({ alter: alterSchema })
 	.then(() => console.log('Database sync successful'))
 	.catch((err) => console.log(err));
 
-const PORT = config.get('Port');
+const PORT = config.get<typeof defaultJson.Port>('Port');
 app.listen(PORT, () => {
 	console.log(`NODE_ENV : ${process.env.NODE_ENV}`);
 	console.log(`listening on port ${PORT}`);
